b343479249b95b6dec725af1c8275629d0df8224
valeri <valera.vorona@gmail.com>
2023-08-19 23:06:49 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <string>
+#include <iostream>
+
+struct Options {
+    enum {
+        CREATE
+    } cmd;
+
+    enum {
+        CLASS
+    } what;
+
+    void run();
+
+private:
+    void create();
+};
+
+void Options::run() {
+    switch (cmd) {
+        case CREATE:
+            create();
+            break;
+    }
+}
+
+void Options::create() {
+    switch (what) {
+        case CLASS:
+            break;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    using namespace std;
+
+    Options o;
+
+    for (int i=1; i< argc; ++i) {
+        cout << argv[i] << endl;
+    }
+
+    if (argc > 2) {
+        if ("create" == argv[1]) {
+            o.cmd = o.CREATE;
+            if (argc > 3) {
+                if ("class" == argv[2]) {
+                    o.what = o.CLASS;
+                }
+            }
+        }
+    }
+
+    o.run();
+
+    return 0;
+}
+
